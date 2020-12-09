931c35b4983856b2333002d8dcd5c30e5174fe19
Thomas Bianco <thomas.bianco@student-cs.fr>
2020-12-09 21:53:17 +0100

    cleanup

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 
 function GoogleMap(props: {
   handleResults: (results: google.maps.places.PlaceResult[]) => void;
